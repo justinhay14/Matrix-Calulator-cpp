ba017d731d1e330ac0bc20ad2909156a3b819eeb
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2020-06-17 16:04:19 -0400

    added rows setter

--- matrix.h
+++ matrix.h
@@ -1,10 +1,20 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+#include <stdlib.h>
+#include <vector>
 
+using std::vector;
 
 class matrix
 {
+private:
+    int rows;
+    int columns;
 public:
+    vector<vector<double> > grid;
+    int get_rows() {
+        return rows;
+    }
     matrix();
 };
 
