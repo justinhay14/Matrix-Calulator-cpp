3baa18336cee9ba283b7e95502c6baad05cfcae4
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2020-07-02 17:35:13 -0400

    inverse works

--- Matrix.h
+++ Matrix.h
@@ -139,18 +139,18 @@ public:
             }
         }
         Matrix manip = Matrix(rows, 2 * columns, manip_grid);
-        manip.print();
+        //manip.print();
         manip = manip.rref();
-        manip.print();
+        //manip.print();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                if (manip.grid[i][j] != 0) {
+                if ((manip.grid[i][j] != 0 && i != j) || (manip.grid[i][j] != 1 && i == j)) {
                     //manip.~Matrix();
                     return Matrix();
                 }
             }
         }
-        cout << "passed\n";
+        //cout << "passed\n";
         manip.set_columns(columns);
         for (int i = 0; i < rows; i++) {
             manip.grid[i].erase(manip.grid[i].begin(), manip.grid[i].begin() + columns);
